6a2767592c96c20c37deb3e42393889dbbecc3f0
huym1nh <minh191103@gmail.com>
2025-09-26 15:30:10 +0700

    test
    test

--- BatTrangHamony/Pages/Home/Test.cshtml.cs
+++ BatTrangHamony/Pages/Home/Test.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace BatTrangHamony.Pages.Login
+namespace BatTrangHamony.Pages.Home
 {
     public class TestModel : PageModel
     {
